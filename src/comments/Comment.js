5c73bac830183bfcb2b4fb581cbb4ba8bb571a2d
Rohit Vishwakarma <53476944+Rohit77707@users.noreply.github.com>
2022-06-26 17:32:28 +0530

    Add files via upload

---
+++ src/comments/Comment.js
@@ -0,0 +1,111 @@
+import CommentForm from "./CommentForm";
+import '../App.css'
+const Comment = ({
+  comment,
+  replies,
+  setActiveComment,
+  activeComment,
+  updateComment,
+  deleteComment,
+  addComment,
+  parentId = null,
+  currentUserId,
+}) => {
+  const isEditing =
+    activeComment &&
+    activeComment.id === comment.id &&
+    activeComment.type === "editing";
+  const isReplying =
+    activeComment &&
+    activeComment.id === comment.id &&
+    activeComment.type === "replying";
+  const fiveMinutes = 300000;
+  const timePassed = new Date() - new Date(comment.createdAt) > fiveMinutes;
+  const canDelete =
+    currentUserId === comment.userId && replies.length === 0 && !timePassed;
+  const canReply = Boolean(currentUserId);
+  const canEdit = currentUserId === comment.userId && !timePassed;
+  const replyId = parentId ? parentId : comment.id;
+  const createdAt = new Date(comment.createdAt).toLocaleDateString();
+  return (
+    <div key={comment.id} className="comment">
+      <div className="comment-image-container">
+      <img className="image" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+      </div>
+      <div className="comment-right-part">
+        <div className="comment-content">
+          <div className="comment-author">{comment.username}</div>
+          <div>{createdAt}</div>
+        </div>
+        {!isEditing && <div className="comment-text">{comment.body}</div>}
+        {isEditing && (
+          <CommentForm
+            submitLabel="Update"
+            hasCancelButton
+            initialText={comment.body}
+            handleSubmit={(text) => updateComment(text, comment.id)}
+            handleCancel={() => {
+              setActiveComment(null);
+            }}
+          />
+        )}
+        <div className="comment-actions">
+          {canReply && (
+            <div
+              className="comment-action"
+              onClick={() =>
+                setActiveComment({ id: comment.id, type: "replying" })
+              }
+            >
+              Reply
+            </div>
+          )}
+          {canEdit && (
+            <div
+              className="comment-action"
+              onClick={() =>
+                setActiveComment({ id: comment.id, type: "editing" })
+              }
+            >
+              Edit
+            </div>
+          )}
+          {canDelete && (
+            <div
+              className="comment-action"
+              onClick={() => deleteComment(comment.id)}
+            >
+              Delete
+            </div>
+          )}
+        </div>
+        {isReplying && (
+          <CommentForm
+            submitLabel="Reply"
+            handleSubmit={(text) => addComment(text, replyId)}
+          />
+        )}
+        {replies.length > 0 && (
+          <div className="replies">
+            {replies.map((reply) => (
+              <Comment
+                comment={reply}
+                key={reply.id}
+                setActiveComment={setActiveComment}
+                activeComment={activeComment}
+                updateComment={updateComment}
+                deleteComment={deleteComment}
+                addComment={addComment}
+                parentId={comment.id}
+                replies={[]}
+                currentUserId={currentUserId}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
